0d861ac5bc7c30a3a6a7507b5b36f7d02a9ebf23
mrshihabullah <68374369+mrshihabullah@users.noreply.github.com>
2021-03-28 18:08:31 +0200

    client file initial commit

---
+++ kv_db_client.erl
@@ -0,0 +1,23 @@
+-module(kv_db_client).
+-author("Shihab").
+
+-export([create/2, update/2, getValueByKey/1, deleteValueByKey/1, totalNumberOfKey/0]).
+
+% start() ->
+%     kv_db_server:start().
+% stop() ->
+%     kv_db_server:stop().
+create(Key, Value) ->
+    kv_db_server:put(Key, Value).
+
+update(Key, Value) ->
+    kv_db_server:put(Key, Value).
+
+getValueByKey(Key) ->
+    kv_db_server:get(Key).
+
+deleteValueByKey(Key) ->
+    kv_db_server:delete(Key).
+
+totalNumberOfKey() ->
+    kv_db_server:size().
