67cb11b46b81dac36ef5ecd458983d71f50f7a62
MikPos <38103474+MikPos@users.noreply.github.com>
2021-04-08 10:31:30 +0200

    Calls no longer start children, and we can do remote calls.

--- kv_db_client.erl
+++ kv_db_client.erl
@@ -6,35 +6,55 @@
 %     kv_db_server:start().
 % stop() ->
 %     kv_db_server:stop().
+
+% -------------------------------------------
+% Internal Calls
+% -------------------------------------------
 create(Key, Value) ->
-    kv_db_supervisor:startChild(Key),
-    db_server:put(Key, Value, Key),
-    kv_db_supervisor:stopChild(Key).
+    % kv_db_supervisor:startChild(Key),
+    db_server:put(Key, Value, Key).
+    % kv_db_supervisor:stopChild(Key).
 
 update(Key, Value) ->
-    kv_db_supervisor:startChild(Key),
+    % kv_db_supervisor:startChild(Key),
     db_server:delete(Key, Key),
-    db_server:put(Key, Value, Key),
-    kv_db_supervisor:stopChild(Key).
+    db_server:put(Key, Value, Key).
+    % kv_db_supervisor:stopChild(Key).
 
 get(Key) ->
-    kv_db_supervisor:startChild(Key),
-    Value = db_server:get(Key, Key),
-    kv_db_supervisor:stopChild(Key),
-    Value.
+    % kv_db_supervisor:startChild(Key),
+    db_server:get(Key, Key).
+    % kv_db_supervisor:stopChild(Key),
+    % Value.
 
 delete(Key) ->
-    kv_db_supervisor:startChild(Key),
-    db_server:delete(Key, Key),
-    kv_db_supervisor:stopChild(Key).
+    % kv_db_supervisor:startChild(Key),
+    db_server:delete(Key, Key).
+    % kv_db_supervisor:stopChild(Key).
 
 countKeys() ->
-    kv_db_supervisor:startChild(?MODULE),
-    Size = db_server:size(?MODULE),
-    kv_db_supervisor:stopChild(?MODULE),
-    Size.
+    % kv_db_supervisor:startChild(?MODULE),
+    db_server:size(?MODULE).
+    % kv_db_supervisor:stopChild(?MODULE),
+    % Size.
 
+% -------------------------------------------
+% Remote Calls
+% -------------------------------------------
+remote_create(Server, Key, Value) ->
+    rpc:call(Server, kv_db_client, create, [Key, Value]).
 
+remote_update(Server, Key, Value) ->
+    rpc:call(Server, kv_db_client, update, [Key, Value]).
+
+remote_get(Server, Key) ->
+    rpc:call(Server, kv_db_client, get, [Key]).
+
+remote_delete(Server, Key) ->
+    rpc:call(Server, kv_db_client, delete, [Key]).
+
+remote_size(Server) ->
+    rpc:call(Server, kv_db_client, countKeys, []).
 
 %% erl -sname 'server' -setcookies 1234
 %% kv_db_supervisor:start_link_from_shell().
