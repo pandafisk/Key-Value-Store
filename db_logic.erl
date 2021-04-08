cdd1f0cc879c0516f3417e68a39027b1f9aa9c07
mrshihabullah <shihabullah13@gmail.com>
2021-04-08 13:39:18 +0200

    comment about delete replica

--- db_logic.erl
+++ db_logic.erl
@@ -59,10 +59,9 @@ addReplica(NodeName) ->
     mnesia:change_table_copy_type(schema, NodeName, disc_copies),
     %To make new the node capable of storing disc copies
     mnesia:add_table_copy(kv_db, NodeName, disc_copies).
-
+%stop Mnesia on Nodename first and then call this method from any other running server node
 removeReplica(Nodename) ->
-    %login to the cluster to be removed
-    %stop mnesia
+    %Now switch to any other node in the cluster and do:
     mnesia:del_table_copy(schema, Nodename).
 
 
