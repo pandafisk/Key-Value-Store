0969aef30880533d802ece086f9c36f690b58152
Troels _Have <37349497+pandafisk@users.noreply.github.com>
2021-04-08 12:57:46 +0200

    (Crude) load balancing

--- kv_db_client.erl
+++ kv_db_client.erl
@@ -1,38 +1,79 @@
 -module(kv_db_client).
 
+%% Internal
 -export([create/2, update/2, get/1, delete/1, countKeys/0]).
+%% Remote
+-export([remote_create/3, remote_update/3, remote_get/2, remote_delete/2, remote_size/1]).
+%% Helpers
+-export([get_server/0, connect_client/1]).
+
+
+%% ======================================================
+%%               Internal Calls
+%% ======================================================
 
 % start() ->
 %     kv_db_server:start().
 % stop() ->
 %     kv_db_server:stop().
 create(Key, Value) ->
-    kv_db_supervisor:startChild(Key),
-    db_server:put(Key, Value, Key),
-    kv_db_supervisor:stopChild(Key).
+    % kv_db_supervisor:startChild(Key),
+    db_server:put(Key, Value).
+    % kv_db_supervisor:stopChild(Key).
 
 update(Key, Value) ->
-    kv_db_supervisor:startChild(Key),
-    db_server:delete(Key, Key),
-    db_server:put(Key, Value, Key),
-    kv_db_supervisor:stopChild(Key).
+    % kv_db_supervisor:startChild(Key),
+    db_server:delete(Key),
+    db_server:put(Key, Value).
+    % kv_db_supervisor:stopChild(Key).
 
 get(Key) ->
-    kv_db_supervisor:startChild(Key),
-    Value = db_server:get(Key, Key),
-    kv_db_supervisor:stopChild(Key),
-    Value.
+    % kv_db_supervisor:startChild(Key),
+    db_server:get(Key).
+    % kv_db_supervisor:stopChild(Key),
+    % Value.
 
 delete(Key) ->
-    kv_db_supervisor:startChild(Key),
-    db_server:delete(Key, Key),
-    kv_db_supervisor:stopChild(Key).
+    % kv_db_supervisor:startChild(Key),
+    db_server:delete(Key).
+    % kv_db_supervisor:stopChild(Key).
 
 countKeys() ->
-    kv_db_supervisor:startChild(?MODULE),
-    Size = db_server:size(?MODULE),
-    kv_db_supervisor:stopChild(?MODULE),
-    Size.
+    % kv_db_supervisor:startChild(?MODULE),
+    db_server:size().
+    % kv_db_supervisor:stopChild(?MODULE),
+    % Size.
+
+%% ======================================================
+%%              Remote Calls
+%% ======================================================
+
+remote_create(Server, Key, Value) ->
+    rpc:call(Server, kv_db_client, create, [Key, Value]).
+
+remote_update(Server, Key, Value) ->
+    rpc:call(Server, kv_db_client, update, [Key, Value]).
+
+remote_get(Server, Key) ->
+    rpc:call(Server, kv_db_client, get, [Key]).
+
+remote_delete(Server, Key) ->
+    rpc:call(Server, kv_db_client, delete, [Key]).
+
+remote_size(Server) ->
+    rpc:call(Server, kv_db_client, countKeys, []).
+
+%% ======================================================
+%%              Helpers
+%% ======================================================
+
+connect_client(Host) ->
+    rpc:call(Host, kv_db_client, get_server, []).
+
+get_server() ->
+    Servers = mnesia:table_info(kv_db, where_to_write),
+    lists:nth(rand:uniform(length(Servers)), Servers).
+
 
 
 
@@ -42,4 +83,3 @@ countKeys() ->
 %%=========== In another shell ==============
 %% erl -sname 'client' -setcookies 1234
 %% rpc:call('name', kv_db_client, 'method', [Args]).
- 
